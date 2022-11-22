85b0a9557d516b2571de07de54a21f0891b3476b
Joshua Tenny <joshuaLeeTenny@gmail.com>
2022-11-22 23:52:15 +0000

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('The first element of this array is:', firstElement);
+
+var thirdElement = languages[3];
+console.log('The third element of this array is:', thirdElement);
+
+var length = languages;
+console.log('The length of the array is:', length.length);
+
+var lastElement = languages;
+console.log('The last element of the array is:', languages[lastElement.length - 1]);
