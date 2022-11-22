2ea4ac0a16d4b8a944781a11b184a4b779fff12c
Joshua Tenny <joshuaLeeTenny@gmail.com>
2022-11-22 01:05:41 +0000

    completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,18 @@
+var person = {
+  firstName: 'Josh',
+  lastName: 'Tenny',
+  hobby: 'Jujitsu'
+
+};
+
+console.log(person);
+
+var fullName = ' My full name is ' + 'Joshua ' + 'Tenny.';
+console.log(fullName);
+
+person.job = ' Web Developer';
+
+console.log('Current job: ' + person.job);
+
+person.previousjob = ' Delivery Driver ';
+console.log('Old job: ' + person.previousjob);
