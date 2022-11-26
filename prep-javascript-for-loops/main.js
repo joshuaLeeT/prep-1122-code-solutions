fd4811cfade488023074bbdced2f536d719f862d
Joshua Tenny <joshuaLeeTenny@gmail.com>
2022-11-26 00:20:21 +0000

    completed-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 11; i += 2) {
+  console.log(i);
+}
+
+for (let i = 0; i < 100; ++i) {
+  console.log('Time till explosion:', i);
+}
